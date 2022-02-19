bb3f6af9ecd65c96722f6b71e515cd9f6182d532
Francesco Catalfamo <99168620+FCatalfamo@users.noreply.github.com>
2022-02-19 11:31:48 +0100

    :sparkle: extract links

--- src/02_scraping.R
+++ src/02_scraping.R
@@ -7,3 +7,16 @@ site <- RCurl::getURL(mare,
                       httpheader = c(From = email, `User-Agent` = UA))
 
 site_parsed <- read_html(site)
+
+extract_links <-  read_html(site) %>%
+  html_elements(xpath = "//a/@href") %>%
+  html_text()
+
+df_links <-  tibble(
+  link = ""
+)
+
+for(i in 1:length(extract_links)) {
+  df_links <-  rbind(df_links, extract_links[i])
+}
+
