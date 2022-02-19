89b0ab1699904fe4a687da04a367f8c7f52ad1ca
Nicola Casarin <nico.casarin@icloud.com>
2022-02-19 15:33:44 +0100

    :memo: Comments on 02_scraping / Regex pattern

--- src/02_scraping.R
+++ src/02_scraping.R
@@ -3,20 +3,24 @@ mare <- "https://beppegrillo.it/un-mare-di-plastica-ci-sommergera/"
 email <-  "test@test.com"
 UA <-  R.Version()$version.string
 
+#download site with specified headers
 site <- RCurl::getURL(mare, 
                       httpheader = c(From = email, `User-Agent` = UA))
 
-site_parsed <- read_html(site)
-
-extract_links <-  read_html(site) %>%
-  html_elements(xpath = "//a/@href") %>%
+#extraction of all link in the webpage
+extract_links  <- read_html(site) %>% 
+  html_elements(xpath = "//a/@href") %>% 
   html_text()
 
-df_links <-  tibble(
+#creating empty df_links
+df_links <- tibble(
   link = ""
 )
 
-for(i in 1:length(extract_links)) {
-  df_links <-  rbind(df_links, extract_links[i])
+#for loop appending the chr vector of links to the df
+for (i in 1:length(extract_links)) {
+  df_links <- rbind(df_links, extract_links[i])
 }
-
+df_links <- df_links %>% 
+  transmute(link = str_extract(df_links$link, pattern = "^https?://beppegrillo.it/.*")) %>% 
+  filter(!is.na(link))
